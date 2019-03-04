c2cc2e1adec3c9efe1db1e862ab6e06d88193608
OscarQQ <qyx1118@outlook.com>
2019-03-03 19:40:50 -0800

    windows to let users select start date and end date

--- Mikado/Mikado/Controller/AddTripViewController.swift
+++ Mikado/Mikado/Controller/AddTripViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class AddTripViewController: UIViewController {
+    
+    
+    @IBOutlet weak var end_date: UITextField!
+    @IBOutlet weak var start_date: UITextField!
+    private var start_datepicker: UIDatePicker?
+    private var end_datepicker: UIDatePicker?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        start_datepicker = UIDatePicker()
+        start_datepicker?.datePickerMode = .date
+        start_datepicker?.addTarget(self, action: #selector(AddTripViewController.startdatechanged(datepicker:)), for: .valueChanged)
+        
+        end_datepicker = UIDatePicker()
+        end_datepicker?.datePickerMode = .date
+        end_datepicker?.addTarget(self, action: #selector(AddTripViewController.enddatechanged(datepicker:)), for: .valueChanged)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(AddTripViewController.viewtapped(gestureRecognizer:)))
+        view.addGestureRecognizer(tap)
+        start_date.inputView = start_datepicker
+        end_date.inputView = end_datepicker
+        
     }
+    
+    @objc func viewtapped(gestureRecognizer:UITapGestureRecognizer){
+        view.endEditing(true)
+    }
+    
+    @objc func startdatechanged(datepicker:UIDatePicker){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MMM-yyyy"
+        start_date.text = formatter.string(from: start_datepicker!.date)
+        view.endEditing(true)
+    }
+    
+    @objc func enddatechanged(datepicker:UIDatePicker){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MMM-yyyy"
+        end_date.text = formatter.string(from: end_datepicker!.date)
+        view.endEditing(true)
+    }
+
     
 
     /*
