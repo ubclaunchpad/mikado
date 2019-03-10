08687debd93f16e99a66cf6ed7f36e96052e63eb
andixiong <xiongandi@gmail.com>
2019-03-09 16:30:43 -0800

    ok

--- Mikado/Mikado/Controller/TripTableViewController.swift
+++ Mikado/Mikado/Controller/TripTableViewController.swift
@@ -11,45 +11,57 @@ import UIKit
 class TripTableViewController: UITableViewController {
     
     var trips : [Trip]?
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupTrips()
         self.navigationItem.title = "Trips"
         
-        tableView.register(TripCell.self, forCellReuseIdentifier: "tripCell")
+        tableView.register(UINib(nibName: "TripCell", bundle: nil), forCellReuseIdentifier: "tripCell")
         tableView.tableFooterView = UIView()
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let tripCell = tableView.dequeueReusableCell(withIdentifier: "tripCell", for: indexPath)
+        let tripCell = tableView.dequeueReusableCell(withIdentifier: "tripCell", for: indexPath) as! TripCell
         
-        let trip = trips?[indexPath.row]
-
-        tripCell.textLabel?.text = trip?.title
-        
+        let trip = trips?[indexPath.section]
+        tripCell.setImage(UIImage(named: "seattle-destination")!)
+        tripCell.setLabel((trip?.title)!)
         return tripCell
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         if let count = trips?.count {
             return count
         }
         return 0
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20;
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "showDay", sender: self)
+        performSegue(withIdentifier: "showDays", sender: self)
     }
     
     // MARK: - Populate the array of trips
     
     func setupTrips() {
-        let trip1 = Trip(title: "Trip1")
-        let trip2 = Trip(title: "Trip2")
+        let samplePlace = Place(name: "San Francisco", coordinates: nil)
+        let trip1 = Trip(title: "Trip1", startDate: Date(), endDate: Date(), destination: samplePlace, days: [])
+        let trip2 = Trip(title: "Trip2", startDate: Date(), endDate: Date(), destination: samplePlace, days: [])
         trips = [trip1, trip2]
     }
     
--- Mikado/Mikado/Model/TripCell.swift
+++ Mikado/Mikado/Model/TripCell.swift
@@ -5,30 +5,53 @@
 //  Created by Aziz Zaynutdinov on 2/10/19.
 //  Copyright © 2019 Aziz Zaynutdinov. All rights reserved.
 //
-
 import UIKit
 
 class TripCell: UITableViewCell {
-
+    
+    @IBOutlet weak var cityImage: UIImageView!
+    @IBOutlet weak var tripName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 4
+        self.clipsToBounds = true
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        backgroundColor = .yellow
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+        //fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setLabel (_ name: String) {
+        tripName.text = name
+        tripName.textColor = .white
+        tripName.font = UIFont.systemFont(ofSize: 20.0)
+    }
+    
+    func setImage (_ im : UIImage) {
+        // set image
+        cityImage.image = im
+        cityImage.layer.opacity = 0.9
+        
+        // dim the image
+        let coverLayer = CALayer()
+        coverLayer.frame = self.bounds;
+        coverLayer.backgroundColor = UIColor.black.cgColor
+        coverLayer.opacity = 0.5
+        cityImage.layer.addSublayer(coverLayer)
     }
     
 }
